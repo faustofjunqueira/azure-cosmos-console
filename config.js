9e3f936ee5f36c5a695eab736654ed04107152b7
RADIXENGRJ\fausto.junqueira <Fausto Junqueira>
2022-03-04 20:24:06 -0300

    + bulk

--- config.js
+++ config.js
@@ -4,6 +4,11 @@ module.exports = {
             "endpoint": "https://xtudo-dev-cosmos.documents.azure.com",
             "key": "rthnVmrYXpenjMSuqhW1Ks8tJ7uA0KPut0E8NzsXHpnw8VRkrTEedmpLn1Y2o4ttI7FdLzJmIBVE2pMoKL0quQ==",
             "database": "xtudo"
+        },
+        "prod": {
+            "endpoint": "https://cosmos-xtudo.documents.azure.com:443/",
+            "key": "P0tmzwIJWR1bL0VlBN7OqsxlnVZzKDFgmiBGwuuat4QDkgiBoBu7Qn8GBHhDeZHLjoHaWaV6uQGrG4DOZBgbWQ==",
+            "database": "xtudo"
         }
     },
     "azure": {
--- index.js
+++ index.js
@@ -1,3 +1,20 @@
+const migrate = require('./lib/migrate');
+
 require('./lib')(async (db) => {
-    // ... write your code
+	// await migrate( db().container("_checkin_form_question"), db("prod").container("_checkin_form_question"));
+  // await migrate( db().container("_checkin_health_check"), db("prod").container("_checkin_health_check"));
+  // await migrate( db().container("_checkin_office_sector"), db("prod").container("_checkin_office_sector"));
+  // await migrate( db().container("_checkin_office_status"), db("prod").container("_checkin_office_status"));
+  // await migrate( db().container("_checkin_reserve"), db("prod").container("_checkin_reserve"));
+  // await migrate( db().container("_checkin_table"), db("prod").container("_checkin_table"));
+  // await migrate( db().container("_checkin_user_privileges"), db("prod").container("_checkin_user_privileges"));
+
+  // await db("prod").container("_checkin_form_question").query("select * from c");
+  // await db("prod").container("_checkin_office_sector").query("select * from c");
+  // await db("prod").container("_checkin_office_status").query("select * from c");
+  // await db("prod").container("_checkin_table").query("select * from c");
+  // await db("prod").container("_checkin_user_privileges").query("select * from c");
+  await db("prod").container("_checkin_reserve").deleteByQuery("select * from c");
+  await db("prod").container("_checkin_health_check").deleteByQuery("select * from c");
+
 })
